fbbed615d778f10ae269ac34856f1ce671ff796c
Mickael Pham <mickael.pham@zuora.com>
2012-08-10 16:19:08 -0700

    Added the resources/images folder

--- src/main/java/eu/mpham/maze/App.java
+++ src/main/java/eu/mpham/maze/App.java
@@ -1,9 +1,24 @@
 package eu.mpham.maze;
 
+/**
+ * This class represents the main entry point for the Maze application.
+ * @author Mickael Pham <mickael.pham@outlook.com>
+ */
 public class App {
-
-	public static void main(String[] args) {
-		Maze maze = new Maze(5, 9);
+	
+	/** Size of the maze (height) */
+	public static final int ROWS = 5;
+	
+	/** Size of the maze (width) */
+	public static final int COLUMNS = 9;
+	
+	private Maze maze;
+	
+	public App() {
+		this.maze = new Maze(ROWS, COLUMNS);
+	}
+	
+	public void proceed() {
 		for (int row = 1; row <= maze.getRows(); row ++) {
 			for (int column = 1; column <= maze.getColumns(); column++) {
 				if (column == maze.getColumns())
@@ -12,6 +27,10 @@ public class App {
 					System.out.print(maze.getType(row, column) + ", ");
 			}
 		}
+	}
+
+	public static void main(String[] args) {
+		new App().proceed();
 	}
 
 }
---
+++ src/main/java/eu/mpham/maze/gui/MainFrame.java
@@ -0,0 +1,10 @@
+package eu.mpham.maze.gui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	
+}
